da08dee65408bad637db2a044c3972575d6ccfe7
Kyung-jun,Park <fdelpini@ncsoft.com>
2020-03-10 21:39:00 +0900

    Remove LegoAPI annotation.

--- app/src/main/java/com/elifox/legocatalog/api/LegoService.kt
+++ app/src/main/java/com/elifox/legocatalog/api/LegoService.kt
@@ -1,6 +1,5 @@
 package com.elifox.legocatalog.api
 
-import com.elifox.legocatalog.di.LegoAPI
 import com.elifox.legocatalog.legoset.data.LegoSet
 import com.elifox.legocatalog.legotheme.data.LegoTheme
 import retrofit2.Response
@@ -17,20 +16,17 @@ interface LegoService {
         const val ENDPOINT = "https://rebrickable.com/api/v3/"
     }
 
-    @LegoAPI
     @GET("lego/themes/")
     suspend fun getThemes(@Query("page") page: Int? = null,
                   @Query("page_size") pageSize: Int? = null,
                   @Query("ordering") order: String? = null): Response<ResultsResponse<LegoTheme>>
 
-    @LegoAPI
     @GET("lego/sets/")
     suspend fun getSets(@Query("page") page: Int? = null,
                         @Query("page_size") pageSize: Int? = null,
                         @Query("theme_id") themeId: Int? = null,
                         @Query("ordering") order: String? = null): Response<ResultsResponse<LegoSet>>
 
-    @LegoAPI
     @GET("lego/sets/{id}/")
     suspend fun getSet(@Path("id") id: String): Response<LegoSet>
 
